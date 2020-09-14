6f922fcdd8e67bb8e0b1b01ee4721cd71e309451
mryanqi2020 <mryanqi2019@163.com>
2020-09-14 12:01:09 +0800

    asdf

--- waifu-tips.js
+++ waifu-tips.js
@@ -127,18 +127,18 @@ function loadWidget(config) {
 		showMessage(text, 7000, 8);
 	})();
 
-	// function showHitokoto() {
-	// 	// 增加 hitokoto.cn 的 API
-	// 	fetch("https://v1.hitokoto.cn")
-	// 		.then(response => response.json())
-	// 		.then(result => {
-	// 			const text = `这句一言来自 <span>「${result.from}」</span>，是 <span>${result.creator}</span> 在 hitokoto.cn 投稿的。`;
-	// 			showMessage(result.hitokoto, 6000, 9);
-	// 			setTimeout(() => {
-	// 				showMessage(text, 4000, 9);
-	// 			}, 6000);
-	// 		});
-	// }
+	function showHitokoto() {
+		// 增加 hitokoto.cn 的 API
+		fetch("https://v1.hitokoto.cn")
+			.then(response => response.json())
+			.then(result => {
+				const text = `这句一言来自 <span>「${result.from}」</span>，是 <span>${result.creator}</span> 在 hitokoto.cn 投稿的。`;
+				showMessage(result.hitokoto, 6000, 9);
+				setTimeout(() => {
+					showMessage(text, 4000, 9);
+				}, 6000);
+			});
+	}
 
 	function showMessage(text, timeout, priority) {
 		if (!text || (sessionStorage.getItem("waifu-text") && sessionStorage.getItem("waifu-text") > priority)) return;
